1004dbb1e4f62ffbe1a7adee9bee142820dda35a
Yash Mandloi <130563219+yashmandloi04@users.noreply.github.com>
2024-12-20 19:01:43 +0530

    Update Paths.jsx

--- client/src/Helpers/Paths.jsx
+++ client/src/Helpers/Paths.jsx
@@ -1,2 +1,2 @@
-export const API_URL = 'https://socialmediaalphaver-api.onrender.com/api/v1'
-export const API_PATH = 'https://socialmediaalphaver-api.onrender.com'
+export const API_URL = 'https://socialmediaalphaver-backend.onrender.com/api/v1'
+export const API_PATH = 'https://socialmediaalphaver-backend.onrender.com'
